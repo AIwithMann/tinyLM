171aa4f800524b4969a92c996b8ab5640758721c
Mann Makwana <aiwithmann@gmail.com>
2025-12-16 12:51:09 +0530

    Enhance tokenizer setup with new features
    Refactor tokenizer initialization and add decoder.

--- tokenizer.py
+++ tokenizer.py
@@ -1,14 +1,37 @@
-from tokenizers import Tokenizer, models, normalizers, pre_tokenizers, trainers, processors
+from tokenizers import (
+    Tokenizer,
+    models,
+    normalizers,
+    pre_tokenizers,
+    trainers,
+    processors,
+    decoders
+)
 
-tokenizer = Tokenizer(models.BPE())
+tokenizer = Tokenizer(
+    models.BPE(unk_token="<unk>")
+)
 
 tokenizer.normalizer = normalizers.NFKC()
 tokenizer.pre_tokenizer = pre_tokenizers.ByteLevel(add_prefix_space=True)
 
 trainer = trainers.BpeTrainer(
     vocab_size=8000,
-    special_tokens=["<pad>","<unk>", "<bos>", "<eos>"]
+    special_tokens=["<pad>", "<unk>", "<bos>", "<eos>", "<sep>"]
 )
+
 tokenizer.train(["text_corpus.txt"], trainer)
-tokenizer.post_processor = processors.ByteLevel(trim_offsets=True)
+
+tokenizer.post_processor = processors.TemplateProcessing(
+    single="<bos> $A <eos>",
+    pair="<bos> $A <sep> $B <eos>",
+    special_tokens=[
+        ("<bos>", tokenizer.token_to_id("<bos>")),
+        ("<eos>", tokenizer.token_to_id("<eos>")),
+        ("<sep>", tokenizer.token_to_id("<sep>")),
+    ],
+)
+
+tokenizer.decoder = decoders.ByteLevel()
+
 tokenizer.save("tokenizer.json")
